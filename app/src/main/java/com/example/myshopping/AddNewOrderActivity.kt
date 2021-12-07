5548cfb20a4f69c1b52e91eb7cce41fc09a7c812
Koreshkov Kostyantyn <konstantin.koresh@gmail.com>
2021-12-07 21:10:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myshopping/AddNewOrderActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myshopping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNewOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_order)
+    }
+}
---
+++ app/src/main/java/com/example/myshopping/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myshopping
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var shoppingBagImageButton: ImageButton
+//    private lateinit var shoppingBagImageButton: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        shoppingBagImageButton = findViewById(R.id.shoppingBagImageButton)
+
+        var shoppingBagImageButton: ImageButton = findViewById(R.id.shoppingBagImageButton)
+
+        shoppingBagImageButton.setOnClickListener{
+            val shoppingBagImageButtonIntent = Intent(this@MainActivity, RegistrationActivity::class.java)
+            startActivity(shoppingBagImageButtonIntent)
+        }
+    }
+}
